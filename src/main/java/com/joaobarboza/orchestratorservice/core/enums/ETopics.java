fb507b9ada5f97889b3d9af33c6cf16f6fd97160
joaobarboza <joaolucasbarbozaa@gmail.com>
2025-11-15 16:40:03 -0300

    feat: adding a Kafka configuration bean

---
+++ src/main/java/com/joaobarboza/orchestratorservice/core/enums/ETopics.java
@@ -0,0 +1,23 @@
+package com.joaobarboza.orchestratorservice.core.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ETopics {
+
+    START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("base-orchestrator"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAIL("finish-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("product-validation-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAIL("payment-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAIL("inventory-fail"),
+    NOTIFY_ENDING("notify-ending");
+
+    private final String topic;
+}
